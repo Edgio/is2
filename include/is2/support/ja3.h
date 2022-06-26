2fee25bb5e82d9a145199033a0438fae37d6a7a5
tinselcity <reed.morrison@gmail.com>
2022-06-26 15:55:26 -0700

    code cleanup.

--- include/is2/support/ja3.h
+++ include/is2/support/ja3.h
@@ -1,3 +1,12 @@
+//! ----------------------------------------------------------------------------
+//! Copyright Edgio Inc.
+//!
+//! \file:    TODO
+//! \details: TODO
+//!
+//! Licensed under the terms of the Apache 2.0 open source license.
+//! Please refer to the LICENSE file in the project root for the terms.
+//! ----------------------------------------------------------------------------
 #ifndef _JA3_H
 #define _JA3_H
 //! ----------------------------------------------------------------------------
